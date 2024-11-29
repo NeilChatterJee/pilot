387349fd477700c61704286dfda80c4659dad903
Neil Chatterjee <neilchatterjee2021@gmail.com>
2024-11-29 10:35:23 -0500

    Area & Perimeter

---
+++ Area and Perimeter Calc.py
@@ -0,0 +1,13 @@
+
+
+radius = input("Please give radius")
+##area = 3.14 * radius * radius
+##print("area", area)
+
+radius1 = int(radius)
+area1 = 3.14 * radius1 * radius1
+if radius1 >= 10:
+    print("area1 =", area1)
+elif radius1 >= 1:
+    circumference = 3.14 * radius1 * 2
+    print(circumference)
